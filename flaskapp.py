7a44efc465c30ff41fa9151042fef0699444d9cb
Caleb Black <cblacktech@gmail.com>
2020-09-13 14:27:28 -0700

    Added docker support

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, url_for, request, redirect, abort
 from time import sleep
-import logging
+import logging, os
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
 app.config['SECRET_KEY'] = 'sdfbW%$EYGHw$%Hwe4r'
 # app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
-# app.config['DEBUG'] = True
 # logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,7 +20,7 @@ def countdown():
 
 @app.route('/display')
 def display():
-    return render_template('display.html', title="Display Page", style="display.css", name="display page")
+    return render_template('display.html', title="Display Page", style="display.css", name="display page: WIP")
     # return "<script>alert('Hello There');</script>"
 
 # @app.route('/', methods=['GET', 'POST'])
@@ -28,4 +28,4 @@ def display():
 #     time = request.form.get('time', 180)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=port)
