aa13262870211480690592fb742d81bc8559debe
Caleb Black <cblacktech@gmail.com>
2020-09-14 19:14:42 -0700

    Added text heart and edited the text that is sent to the html page

--- flaskapp.py
+++ flaskapp.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, url_for, request, redirect, abort
 from time import sleep
-import logging, os
+import logging
+import os
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 app.config['SECRET_KEY'] = 'sdfbW%$EYGHw$%Hwe4r'
+
+
 # app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
 # logging.basicConfig(level=logging.DEBUG)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def countdown():
@@ -18,14 +22,30 @@ def countdown():
     else:
         return render_template('countdown.html', title="Countdown", style='countdown.css', custom_js='countdown.js')
 
+
 @app.route('/display')
 def display():
-    return render_template('display.html', title="Display Page", style="display.css", name="display page: WIP")
-    # return "<script>alert('Hello There');</script>"
+    text = ""
+    cnt = 4
 
-# @app.route('/', methods=['GET', 'POST'])
-# def custom():
-#     time = request.form.get('time', 180)
+    text += ('-' * 25 * cnt) + '\n'
+    text += ('|' + '  ****            ****  ') * cnt + '|\n'
+    text += ('|' + ' *    **        **    * ') * cnt + '|\n'
+    text += ('|' + '  *      **  **      *  ') * cnt + '|\n'
+    text += ('|' + '   *       **       *   ') * cnt + '|\n'
+    text += ('|' + '    *              *    ') * cnt + '|\n'
+    text += ('|' + '     *            *     ') * cnt + '|\n'
+    text += ('|' + '      *          *      ') * cnt + '|\n'
+    text += ('|' + '       *        *       ') * cnt + '|\n'
+    text += ('|' + '        *      *        ') * cnt + '|\n'
+    text += ('|' + '         *    *         ') * cnt + '|\n'
+    text += ('|' + '          *  *          ') * cnt + '|\n'
+    text += ('|' + '           **           ') * cnt + '|\n'
+    text += ('-' * 25 * cnt) + '\n'
+    # print(text_list)
+    return render_template('display.html', title="Display Page", style="display.css",
+                           top_text="HAPPY BIRTHDAY !!!!", text=text)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=port)
